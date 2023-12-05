5e029e41bee3f2db8522aea33136e0ee167a43cb
Andrii Klubuk <andrii.klubuk@shelf.io>
2023-12-05 11:33:45 +0200

    feat: Lint

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,22 @@
 /** @type {import('jest').Config} */
 export default {
-    collectCoverageFrom: ['src/**/*.ts', '!src/**/schema.ts', '!src/**/types.ts'],
-    coverageThreshold: {
-      global: {
-        branches: 90,
-        functions: 90,
-        lines: 90,
-        statements: 90,
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/schema.ts', '!src/**/types.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
+  reporters: ['default', 'jest-junit'],
+  transform: {
+    '\\.[jt]sx?$': [
+      'babel-jest',
+      {
+        configFile: '@shelf/babel-config/backend',
       },
-    },
-    reporters: ['default', 'jest-junit'],
-    transform: {
-      '\\.[jt]sx?$': [
-        'babel-jest',
-        {
-          configFile: '@shelf/babel-config/backend',
-        },
-      ],
-    },
-    testEnvironment: 'node',
-  };
+    ],
+  },
+  testEnvironment: 'node',
+};
